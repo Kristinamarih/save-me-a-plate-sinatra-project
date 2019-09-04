29603b2ec4a55778aee577ac61e91926be13b308
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-04 13:37:17 +0000

    add controller features

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,26 @@
 class RestaurantsController < ApplicationController
 
+  get '/restaurant/:id' do
+
+  end
+
+  get '/signup' do
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  get '/logout' do
+
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
 
+  get '/user/:id' do
+
+  end
+
+  get '/signup' do
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  get '/logout' do
+
+  end
 end
