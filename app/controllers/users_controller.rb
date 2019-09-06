4137714e8e96f7610e434b9525a9f9dec853e7cf
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-06 19:14:47 +0000

    add helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   get '/user/:id' do
-
+    @user = User.find(params[:id])
+    erb :'/user/show'
   end
 
   get '/signup' do
-
+      
   end
 
   post '/signup' do
