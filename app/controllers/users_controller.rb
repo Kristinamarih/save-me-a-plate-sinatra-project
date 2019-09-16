33c421bb923f45c889e5385fc2bbe22bd4837b1b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-16 06:48:07 +0000

    build user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,14 +24,29 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-
+    if !logged_in?
+      erb :'/user/login'
+    else
+      redirect '/meal/show'
+    end
   end
 
   post '/login' do
-
+    @user = User.find_by(:email => params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/meal/show'
+    else
+      redirect '/user/new'
+    end
   end
 
   get '/logout' do
-
+    if logged_in?
+      session.destroy
+      redirect '/user/new'
+    else
+      redirect '/'
+    end
   end
 end
